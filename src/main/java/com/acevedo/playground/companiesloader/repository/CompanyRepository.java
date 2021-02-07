bb552d1a127eb57591830b2f9f0964bd6c205629
rac@INTRACTIV.LOCAL <racevedo@intractiv.com>
2021-02-07 12:37:58 +0100

    removing unused mongo code

--- src/main/java/com/acevedo/playground/companiesloader/repository/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acevedo.playground.companiesloader.repository;
-
-import com.acevedo.playground.companiesloader.model.CompanyEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<CompanyEntity, String> {
-
-}
